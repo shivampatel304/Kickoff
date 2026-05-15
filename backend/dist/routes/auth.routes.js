9e941d8bb5889f276cc425d2294447763cabe1be
Shivam Patel <shivampatel0304@gmail.com>
2026-05-15 18:57:01 -0400

    CI/CD solve

--- backend/dist/routes/auth.routes.js
+++ backend/dist/routes/auth.routes.js
@@ -7,4 +7,7 @@ const router = (0, express_1.Router)();
 router.post("/signup", auth_controller_1.signup);
 router.post("/login", auth_controller_1.login);
 router.post("/me", auth_1.authenticate, auth_controller_1.getMe);
+router.get("/help", (req, res) => {
+    return res.status(200).json({ msg: "hello" });
+});
 exports.default = router;

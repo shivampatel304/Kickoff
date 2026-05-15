6e3a52fbbe47c25c0a4602dfab60b1ec54eecb1b
Shivam Patel <shivampatel0304@gmail.com>
2026-05-15 18:35:39 -0400

    CI/CD testing

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,15 @@
 import {Router} from "express";
 import {signup, login, getMe} from "../controllers/auth.controller";
 import { authenticate } from "../middleware/auth";
+import {Response, Request} from "express";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/me", authenticate, getMe);
+router.get("/help", (req: Request, res: Response) => {
+    return res.status(200).json({msg: "hello"});
+})
 
 export default router;
